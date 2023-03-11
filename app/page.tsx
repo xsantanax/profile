0e1612cf5ca75a46445fcec608bf329ad14b798e
xsantanax <rafaelsantana111@gmail.com>
2023-03-11 13:26:25 -0300

    past projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+import Link from 'next/link'
+
 function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className='p-10 px-12 text-[#ddd]'>
+      <div>Check out some of my past projects</div>
+      <div className='mt-10 flex gap-8 flex-wrap'>
+        <Link href='https://sort.xyz' target='_blank'>
+          <div className='w-48 h-48 rounded-[40px] flex items-center'>
+            <img className='w-48 h-32' src='img/sort.png' />
+          </div>
+        </Link>
+
+        <Link href='https://highlinefast.com' target='_blank'>
+          <img className=' w-48 h-48 rounded-[40px]' src='img/highline.webp' />
+        </Link>
+
+        <Link href='https://bucketofcrabs.net' target='_blank'>
+          <img className=' w-48 h-48 rounded-[40px]' src='img/boc.jpeg' />
+        </Link>
+
+        <Link href='https://bannerwave.com' target='_blank'>
+          <img
+            className=' w-48 h-48 rounded-[40px]'
+            src='img/bannerwave.jpeg'
+          />
+        </Link>
+
+        <Link href='https://politik.vercel.app/' target='_blank'>
+          <img className=' w-48 h-48 rounded-[40px]' src='img/politik.png' />
+        </Link>
+      </div>
     </div>
   )
 }
