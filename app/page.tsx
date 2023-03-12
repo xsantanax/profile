2abdb7fcd18d74e8ae6bd3b057c413de5dbe9fb0
xsantanax <rafaelsantana111@gmail.com>
2023-03-12 12:25:25 -0300

    add text over image

--- app/page.tsx
+++ app/page.tsx
@@ -19,12 +19,24 @@ function HomePage() {
     <div className='overflow-hidden'>
       {isOnTop ? <HeaderTransparent /> : <Header />}
 
-      <div className='flex justify-center'>
+      <div
+        style={{ backgroundImage: 'url(img/nightcity.jpg)' }}
+        className=' max-w-[100%] bg-center 
+        h-screen w-full min-w-[1440px] min-h-[766px] bg-cover'
+      >
+        <div className='px-16 pt-32 text-[#ddd]'>
+          Hi, I'm Rafael Santana, a full-stack developer from Brazil. I'm
+          passionate about building great software and I'm always looking for
+          new challenges.
+        </div>
+      </div>
+
+      {/* <div className='flex justify-center'>
         <img
           src='img/nightcity.jpg'
           className='h-screen w-full min-w-[1440px] min-h-[766px]'
         />
-      </div>
+      </div> */}
 
       <PastWork />
     </div>
