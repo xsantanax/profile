08a33b157b8644a6cf871e3c2f74dd8ab8aa9d24
xsantanax <rafaelsantana111@gmail.com>
2023-03-12 20:26:40 -0300

    edit about

--- components/About.tsx
+++ components/About.tsx
@@ -2,28 +2,35 @@ function About() {
   return (
     <>
       <div className='py-20 px-12 h-screen w-full flex items-center justify-center'>
-        <div className='w-[860px] flex bg-green-800'>
+        <div className='w-[860px] flex'>
           <div className='flex flex-col w-[600px]'>
-            <div className='mt-8'>
+            <div>
               Hello! My name is Rafael and I enjoy creating the internet. My
               interest in app development started back in 2014 when I decided to
               try building Android apps. Soon I also dived deep in web
               development and I've been in love with it ever since.
             </div>
-            <div className='mt-8'>
-              Fast-forward to today, and I’ve had the privilege of working at
-              startups, banks, and web and app development agencies in Brazil
-              and the United States. My main focus these days is building
-              delightful and digital experiences.
+            <div className='mt-6'>
+              Fast-forward to today, and I’ve had the privilege of working at{' '}
+              <a className='text-blue-400' href='https://sort.xyz'>
+                startups
+              </a>
+              ,{' '}
+              <a className='text-blue-400' href='https://nubank.com.br/en'>
+                banks
+              </a>
+              , and web and app development companies in Brazil and the United
+              States. My main focus these days is building delightful and
+              digital experiences.
             </div>
-            <div className='mt-8'>
+            <div className='mt-6'>
               Also, I am currently searching for long-term opportunities in the
               global development market.
             </div>
-            <div className='mt-8'>
+            <div className='mt-6'>
               Here are a few technologies I’ve been working with recently:
             </div>
-            <div className='flex'>
+            <div className='flex mt-4'>
               <div className='flex flex-col'>
                 <div>Typescript</div>
                 <div>React</div>
@@ -39,7 +46,7 @@ function About() {
               </div>
             </div>
           </div>
-          <div className='h-72 w-72 bg-red-700' />
+          <img src='img/me.jpg' className='h-64 w-64 object-cover ml-14' />
         </div>
       </div>
     </>
