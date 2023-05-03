afd4b37f84fa1125683bc98746935e280225bf5b
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-03 13:27:00 -0300

    header z index

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ function Header() {
 
   return (
     <div
-      className={`fixed space-x-8 h-16 w-full px-10 flex justify-center items-center 
+      className={`fixed space-x-8 h-16 w-full px-10 flex justify-center items-center z-10
     ${isAtTop ? 'border-b-[0.5px] border-[#eee]' : 'bg-[#111]'}
     `}
     >
