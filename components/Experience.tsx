8aa4b407a47a6f94e262e466a6dd91271022fc4c
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-04 00:04:17 -0300

    experience pt1

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -7,41 +7,34 @@ export default function Experience() {
       <SectionHeader title='Experience' />
       <div className='bodyItemContent'>
         <div className='bodyItem'>
-          <div>Check out some of my past projects</div>
-          <div className='mt-10 flex gap-8 flex-wrap'>
-            <Link href='https://sort.xyz' target='_blank'>
-              <div className='w-48 h-48 rounded-[40px] flex items-center hover:animate-pulse'>
-                <img className='w-48 h-32' src='img/sort.png' />
-              </div>
-            </Link>
+          <div className='mt-10 flex gap-10 flex-col'>
+            <div className='flex'>
+              <Link href='https://sort.xyz' target='_blank'>
+                <div className='w-48 h-48 bg-white p-[12px] px-[15px] rounded-[40px] flex items-center hover:animate-pulse'>
+                  <img src='img/sort.png' />
+                </div>
+              </Link>
+              <div className='ml-12 mt-4'>Project description</div>
+            </div>
 
-            <Link href='https://highlinefast.com' target='_blank'>
-              <img
-                className=' w-48 h-48 rounded-[40px] hover:animate-pulse'
-                src='img/highline.webp'
-              />
-            </Link>
+            <div className='flex'>
+              <Link href='https://highlinefast.com' target='_blank'>
+                <img
+                  className=' w-48 h-48 rounded-[40px] hover:animate-pulse'
+                  src='img/highline.webp'
+                />
+              </Link>
+              <div className='ml-12 mt-4'>Project description</div>
+            </div>
 
-            <Link href='https://bucketofcrabs.net' target='_blank'>
-              <img
-                className=' w-48 h-48 rounded-[40px] hover:animate-pulse'
-                src='img/boc.jpeg'
-              />
-            </Link>
-
-            <Link href='https://bannerwave.com' target='_blank'>
-              <img
-                className=' w-48 h-48 rounded-[40px] hover:animate-pulse'
-                src='img/bannerwave.jpeg'
-              />
-            </Link>
-
-            <Link href='https://politik.vercel.app/' target='_blank'>
-              <img
-                className=' w-48 h-48 rounded-[40px] hover:animate-pulse bg-[#111] p-6'
-                src='img/politik.png'
-              />
-            </Link>
+            <div className='flex'>
+              <Link href='https://bucketofcrabs.net' target='_blank'>
+                <div className='w-48 h-48 bg-white p-[12px] rounded-[40px] flex items-center hover:animate-pulse'>
+                  <img src='img/boc.jpeg' />
+                </div>
+              </Link>
+              <div className='ml-12 mt-4'>Project description</div>
+            </div>
           </div>
         </div>
       </div>
