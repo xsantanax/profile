a8f089482c7bcd49080725ad20d07db5b293407e
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-04 18:52:56 -0300

    adjust styles

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -6,50 +6,58 @@ export default function Experience() {
     <div id='experience' className='bodyItemWrapper bg-[#00132E]'>
       <SectionHeader title='Experience' />
       <div className='bodyItemContent'>
-        <div className='bodyItem'>
-          <div className='flex gap-10 flex-col'>
-            <div className='flex'>
-              <Link href='https://sort.xyz' target='_blank'>
-                <div className='w-48 h-48 bg-white p-[12px] px-[16px] rounded-[24px] flex items-center hover:animate-pulse'>
-                  <img src='img/sort.png' />
-                </div>
-              </Link>
-              <div className='ml-12 mt-4'>
-                Sort is a web app developed to enable easy and fast querying of
-                data on the Ethereum ecossystem. Using on an array of blockchain
-                technologies, the platform enables the user to login using their
-                Ethereum wallet and explore their transactions, contracts,
-                tokens, and any Ethereum data they might have interest in,
-                viewing them in clean, beautiful charts.
+        <div className='flex gap-16 flex-col'>
+          <div className='flex gap-12 mb-4'>
+            <Link href='https://sort.xyz' target='_blank'>
+              <div className='w-48 h-48 bg-white p-[12px] rounded-[24px] flex items-center hover:animate-pulse'>
+                <img src='img/sort.png' />
               </div>
+            </Link>
+            <div className='py-2 flex flex-col justify-center'>
+              <div className='font-bold text-[17px] mb-2 underline tracking-[0.5px] cursor-pointer'>
+                Sort.xyz
+              </div>
+              Sort is a web app developed to enable easy and fast querying of
+              data on the Ethereum ecossystem. Using blockchain technologies,
+              the platform enables the user to login using their Ethereum wallet
+              and explore transactions, contracts, tokens, and any Ethereum data
+              they might have interest in, viewing them in clean, beautiful
+              charts.
+            </div>
+          </div>
+
+          <div className='flex gap-12 mb-4'>
+            <div className='py-2 flex flex-col justify-center text-right '>
+              <div className='font-bold text-[17px] mb-2 underline tracking-[0.5px] cursor-pointer'>
+                HighlineFast
+              </div>
+              HighlineFast is an internet provider company, which the website
+              was developed for. Its purpose is to allow new prospects to know
+              more about their product, check for its availability in their
+              address, and order it if it is available.
             </div>
 
-            <div className='flex'>
-              <Link href='https://highlinefast.com' target='_blank'>
-                <div className='w-48 h-48 bg-white p-[12px] rounded-[24px] flex items-center hover:animate-pulse'>
-                  <img src='img/highline.webp' />
-                </div>
-              </Link>
-              <div className='ml-12 mt-4'>
-                HighlineFast is an internet provider company, which the website
-                was developed for. Its purpose is to allow new prospects to know
-                more about the product, check for its availability in their
-                address, and order it if it is available.
+            <Link href='https://highlinefast.com' target='_blank'>
+              <div className='w-48 h-48 bg-white p-[12px] rounded-[24px] flex items-center hover:animate-pulse'>
+                <img src='img/highline.webp' />
               </div>
-            </div>
+            </Link>
+          </div>
 
-            <div className='flex'>
-              <Link href='https://bucketofcrabs.net' target='_blank'>
-                <div className='w-48 h-48 bg-white p-[12px] rounded-[24px] flex items-center hover:animate-pulse'>
-                  <img src='img/boc.jpeg' />
-                </div>
-              </Link>
-              <div className='ml-12 mt-4'>
-                Bucket of Crabs is a company that matches Minecraft developers
-                with Minecraft studios. The website purpose is to allow studios
-                to post jobs and allow developers to browse these jobs and apply
-                to ones they like.
+          <div className='flex gap-12 mb-4'>
+            <Link href='https://bucketofcrabs.net' target='_blank'>
+              <div className='w-48 h-48 bg-white p-[12px] rounded-[24px] flex items-center hover:animate-pulse'>
+                <img src='img/boc.jpeg' />
               </div>
+            </Link>
+            <div className='py-2 flex flex-col justify-center '>
+              <div className='font-bold text-[17px] mb-2 underline tracking-[0.5px] cursor-pointer'>
+                Bucket of Crabs
+              </div>
+              Bucket of Crabs is a company that matches Minecraft developers
+              with Minecraft studios. The website purpose is to allow studios to
+              post jobs and allow developers to browse these jobs and apply to
+              ones they like.
             </div>
           </div>
         </div>
