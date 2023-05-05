e2ef691b9565d91cbc929be9a7d33bd3804acfe2
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-05 14:22:02 -0300

    adjust style

--- components/About.tsx
+++ components/About.tsx
@@ -9,28 +9,27 @@ function About() {
           <div>
             Hello! My name is Rafael and I am a software engineer who loves
             building the internet. My interest in app development started back
-            in 2014 when I decided to try building Android apps. Later I also
+            in 2014 when I decided to try building Android apps. Soon I also
             started building web and mobile apps (iOS and Android), and I've
             been working with it ever since.
           </div>
           <div className='mt-6'>
             Fast-forward to today, and I’ve had the privilege of working at
             startups, banks, and web and app development companies in Brazil and
-            the United States. My main focus these days is building delightful
+            the United States. My main focus these days is building amazing
             digital experiences.
           </div>
           <div className='mt-6'>
             Here are a few technologies I’ve been working with recently:
           </div>
           <div className='flex mt-4'>
-            <div className='flex flex-col w-[40%]'>
+            <div className='flex flex-col w-[50%]'>
               <div>Typescript</div>
               <div>React</div>
               <div>React Native</div>
               <div>Next.js</div>
             </div>
-            <div className='flex-1' />
-            <div className='flex flex-col w-[40%]'>
+            <div className='flex flex-col w-[50%]'>
               <div>Firebase</div>
               <div>Git</div>
               <div>Tailwind CSS</div>
