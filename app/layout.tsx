a64a99cfad102bbe9ea4b496ecdd976fdc40d718
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-05 20:44:03 -0300

    remove session/auth providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,4 @@
 import '../styles/globals.css'
-// import { SessionProvider } from '../components/SessionProvider'
-// import { getServerSession } from 'next-auth'
-// import { authOptions } from '../pages/api/auth/[...nextauth]'
-// import ClientProvider from '../components/ClientProvider'
-// import Login from '../components/Login'
 import Header from '../components/Header'
 import HeaderMobile from '../components/HeaderMobile'
 import { Toaster } from 'react-hot-toast'
@@ -13,31 +8,18 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  // const session = await getServerSession(authOptions)
-
   return (
     <html>
       <head />
       <body>
         <Toaster position='top-right' />
-        {/* <SessionProvider session={session}>
-          {!session ? (
-            <Login />
-          ) : ( */}
-        <div className='flex flex-col'>
-          <div className='md:hidden'>
-            <HeaderMobile />
-          </div>
-          <div className='hidden md:block'>
-            <Header />
-          </div>
-          {/* <div className='flex mt-16'> */}
-          {/* <ClientProvider /> */}
-          {children}
+        <div className='md:hidden'>
+          <HeaderMobile />
         </div>
-        {/* </div> */}
-        {/* )}
-        </SessionProvider> */}
+        <div className='hidden md:block'>
+          <Header />
+        </div>
+        {children}
       </body>
     </html>
   )
