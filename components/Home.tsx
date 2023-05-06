26bb0860338daf3da86a8f49ab3a2cf70fa539c2
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-06 20:28:47 -0300

    home responsivity

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,12 +3,15 @@ export default function Home() {
     <div
       id='/'
       style={{ backgroundImage: 'url(img/nightcity.jpg)' }}
-      className='bg-center bg-cover h-screen w-full min-h-[766px] px-16'
+      className='bg-center bg-cover h-screen w-full min-h-[766px] px-16 flex justify-center'
     >
-      <div className='pt-[126px]'>Hi, I am</div>
-      <div className='pt-[10px] text-[68px] text-bold'>Rafael Santana</div>
-      <div className='pt-[10px]'>
-        A software engineer specialized in building amazing digital experiences.
+      <div className='max-w-[1560px] w-full'>
+        <div className='pt-[126px]'>Hi, I am</div>
+        <div className='pt-[10px] text-[68px] text-bold'>Rafael Santana</div>
+        <div className='pt-[10px]'>
+          A software engineer specialized in building amazing digital
+          experiences.
+        </div>
       </div>
     </div>
   )
