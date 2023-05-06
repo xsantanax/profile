6088e8faf954c71a2f5f0e498154823ce0d63466
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-06 16:03:29 -0300

    section header refactor

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -5,7 +5,7 @@ export default function SectionHeader({ title }: Props) {
   return (
     <div className='flex w-full md:w-[860px] md:px-20'>
       <div className='italic font-[600] text-[18px]'>{title}</div>
-      <div className='flex flex-col ml-8 w-full'>
+      <div className='col ml-8 w-full'>
         <div className='h-[60%] border-b-[1px] border-gray-400' />
         <div className='h-[40%]' />
       </div>
