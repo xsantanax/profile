11b5abfbbeac5f2f779b5ac1b2121cd34a96665c
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-06 16:12:37 -0300

    adjust text size

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -3,8 +3,8 @@ type Props = {
 }
 export default function SectionHeader({ title }: Props) {
   return (
-    <div className='flex w-full md:w-[860px] md:px-20'>
-      <div className='italic font-[600] text-[18px]'>{title}</div>
+    <div className='flex w-full md:w-[880px] md:px-20'>
+      <div className='italic font-[600]'>{title}</div>
       <div className='col ml-8 w-full'>
         <div className='h-[60%] border-b-[1px] border-gray-400' />
         <div className='h-[40%]' />
