e48a80492a72ff37286b879b1f77ce208c612631
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-06 17:42:30 -0300

    adjust styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
 
   return (
     <div
-      className={`fixed space-x-8 h-[72px] w-full px-10 flex items-center z-10 font-[400] tracking-[0.5px]
+      className={`fixed space-x-8 h-[72px] w-full px-10 flex items-center z-10
                     ${isAtTop ? 'border-b-[0.5px] border-[#ddd]' : 'bg-[#111]'}
                 `}
     >
