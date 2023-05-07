862a63b0ff523ba2d12b4e7a2a89459db146075e
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-07 18:28:47 -0300

    style

--- components/Home.tsx
+++ components/Home.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       className='bg-center bg-cover h-screen w-full min-h-[766px] px-8 md:px-16 flex justify-center text-shadow-mine shadow-blue-900'
     >
       <div className='max-w-[1560px] w-full '>
-        <div className='pt-[112px] md:pt-[128px] '>Hi, I am</div>
-        <div className='py-[20px] text-[52px] md:text-[60px] font-bold leading-[1.1] '>
+        <div className='pt-[108px] md:pt-[128px] '>Hi, I am</div>
+        <div className='py-[18px] text-[52px] md:text-[60px] font-bold leading-[1.1] '>
           Rafael Santana
         </div>
         <div>
