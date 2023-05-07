b25e6ad481aa02c14f619f1443bb83a3a91c7b2b
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-07 18:06:43 -0300

    add text-shadow + config

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,11 +3,11 @@ export default function Home() {
     <div
       id='/'
       style={{ backgroundImage: 'url(img/nightcity.jpg)' }}
-      className='bg-center bg-cover h-screen w-full min-h-[766px] px-8 md:px-16 flex justify-center'
+      className='bg-center bg-cover h-screen w-full min-h-[766px] px-8 md:px-16 flex justify-center text-shadow-lg shadow-blue-900'
     >
-      <div className='max-w-[1560px] w-full'>
-        <div className='pt-[112px] md:pt-[128px]'>Hi, I am</div>
-        <div className='py-[20px] text-[52px] md:text-[60px] font-bold leading-[1.1]'>
+      <div className='max-w-[1560px] w-full '>
+        <div className='pt-[112px] md:pt-[128px] '>Hi, I am</div>
+        <div className='py-[20px] text-[52px] md:text-[60px] font-bold leading-[1.1] '>
           Rafael Santana
         </div>
         <div>
