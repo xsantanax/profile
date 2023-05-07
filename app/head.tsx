db8c2ca5abf4799ce38f4161154eaa983553d963
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-07 12:37:04 -0300

    add head icon

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Rafael Santana</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <link rel='icon' href='/favicon.ico' />
+      <link rel='icon' href='/img/building.png' />
     </>
   )
 }
