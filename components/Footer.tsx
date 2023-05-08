67b8f2aaad121cadefddc1fec3c9b2791925d0c0
Rafael Santana <rafaelsantana111@gmail.com>
2023-05-07 21:24:48 -0300

    s

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className='p-10 w-full text-center bg-[#030918] text-[17px]'>
+    <div className='p-10 w-full text-center bg-[#030918]'>
       Rafael Santana, {new Date().getFullYear()}
     </div>
   )
