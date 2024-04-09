0562c51dc9733308fb9ba531b21421d20364ab66
xsantanax <rafaelsantana111@gmail.com>
2024-04-09 00:36:26 -0300

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
-import '../styles/globals.css'
-import Header from '../components/Header'
-import HeaderMobile from '../components/HeaderMobile'
-import Footer from '../components/Footer'
-import { Toaster } from 'react-hot-toast'
+import "../styles/globals.css"
+import Header from "../components/Header"
+import HeaderMobile from "../components/HeaderMobile"
+import Footer from "../components/Footer"
+import { Toaster } from "react-hot-toast"
+import { GoogleTagManager } from "@next/third-parties/google"
 
 export default async function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode
 }) {
   return (
     <html>
-      <head />
+      {/* <head /> */}
+      <GoogleTagManager gtmId="GTM-NN4TM9JT" />
       <body>
-        <Toaster position='top-right' />
-        <div className='md:hidden'>
+        <Toaster position="top-right" />
+        <div className="md:hidden">
           <HeaderMobile />
         </div>
-        <div className='hidden md:block'>
+        <div className="hidden md:block">
           <Header />
         </div>
         {children}
