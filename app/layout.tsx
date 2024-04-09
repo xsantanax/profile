548f74f151d761ce688bb87986eced89f436b520
xsantanax <rafaelsantana111@gmail.com>
2024-04-09 00:56:10 -0300

    use next/script

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import HeaderMobile from "../components/HeaderMobile"
 import Footer from "../components/Footer"
 import { Toaster } from "react-hot-toast"
 // import { GoogleTagManager } from "@next/third-parties/google"
+import Script from "next/script"
 
 export default async function RootLayout({
   children,
@@ -13,42 +14,32 @@ export default async function RootLayout({
   return (
     <html>
       <head>
-        {/* Add GTM script */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                <!-- Google Tag Manager -->
-                <script>
-                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-NN4TM9JT');
-                </script>
-                <!-- End Google Tag Manager -->
-              `,
-          }}
-        />
-        {/* <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NN4TM9JT');</script> */}
+        {/* Google Tag Manager */}
+        {process.env.VERCEL_ENV === "production" && (
+          <Script id="google-tag-manager">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NN4TM9JT');
+            `}
+          </Script>
+        )}
       </head>
       {/* <GoogleTagManager gtmId="GTM-NN4TM9JT" /> */}
       <body>
-        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NN4TM9JT"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GTM-NN4TM9JT`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+        {/* Google Tag Manager */}
+        {process.env.VERCEL_ENV === "production" && (
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NN4TM9JT"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+        )}
         <Toaster position="top-right" />
         <div className="md:hidden">
           <HeaderMobile />
