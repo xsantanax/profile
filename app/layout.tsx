31edc46e1c9bd8a096c017c051e58e52a957ee9e
xsantanax <rafaelsantana111@gmail.com>
2024-04-09 00:49:35 -0300

    add google tag manager dangerouslySetInnerHTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,22 @@ export default async function RootLayout({
   return (
     <html>
       <head>
+        {/* Add GTM script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                <!-- Google Tag Manager -->
+                <script>
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-NN4TM9JT');
+                </script>
+                <!-- End Google Tag Manager -->
+              `,
+          }}
+        />
         {/* <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -23,6 +39,16 @@ export default async function RootLayout({
       <body>
         {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NN4TM9JT"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-NN4TM9JT`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Toaster position="top-right" />
         <div className="md:hidden">
           <HeaderMobile />
