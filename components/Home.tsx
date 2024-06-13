ad8912dd836daa42990f3eeb6437d770f871818c
xsantanax <rafaelsantana111@gmail.com>
2024-06-13 15:02:17 -0300

    add onLoad effect

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from 'react'
+
 export default function Home() {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    const img = new Image()
+    img.src = 'img/nightcity.jpg'
+    img.onload = () => setIsLoaded(true)
+  }, [])
   return (
     <div
       id='/'
       style={{ backgroundImage: 'url(img/nightcity.jpg)' }}
-      className='bg-center bg-cover h-screen w-full min-h-[766px] px-8 md:px-16 flex justify-center text-shadow-mine shadow-blue-900 fade-in'
+      className={`bg-center bg-cover h-screen w-full min-h-[766px] px-8 md:px-16 flex justify-center text-shadow-mine shadow-blue-900 ${
+        isLoaded ? 'fade-in' : 'hidden'
+      }`}
     >
       <div className='max-w-[1560px] w-full '>
         <div className='pt-[108px] md:pt-[128px] '>Hi, I am</div>
